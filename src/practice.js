a97278cf3b0be8b8a5e47a0b86cdbe7d00a94501
Kevin Tsai <k.tsai92@gmail.com>
2019-10-14 15:55:50 -0400

    first commit

---
+++ src/practice.js
@@ -0,0 +1,78 @@
+require('dotenv').config()
+const knex = require('knex')
+
+const knexInstance = knex({
+  client: 'pg',
+  connection: process.env.DB_URL
+})
+
+
+console.log('knex and driver installed correctly');
+
+knexInstance.from('amazong_products').select('*')
+	.then(result => {
+		console.log(result)
+	})
+
+	knexInstance
+  .select('product_id', 'name', 'price', 'category')
+  .from('amazong_products')
+  .where({ name: 'Point of view gun' })
+  .then(result => {
+    console.log(result)
+	})
+	
+const searchTerm = 'holo'
+
+function searchByProduceName(searchTerm) {
+knexInstance
+  .select('product_id', 'name', 'price', 'category')
+  .from('amazong_products')
+  .where('name', 'ILIKE', `%${searchTerm}%`)
+  .then(result => {
+    console.log(result)
+	})
+}
+
+searchByProduceName('holo')
+
+function paginateProducts(page) {
+  const productsPerPage = 10
+  const offset = productsPerPage * (page - 1)
+  knexInstance
+    .select('product_id', 'name', 'price', 'category')
+    .from('amazong_products')
+    .limit(productsPerPage)
+    .offset(offset)
+    .then(result => {
+      console.log(result)
+    })
+}
+
+paginateProducts(2)
+
+function getProductsWithImages() {
+  knexInstance
+    .select('product_id', 'name', 'price', 'category', 'image')
+    .from('amazong_products')
+    .whereNotNull('image')
+    .then(result => {
+      console.log(result)
+    })
+}
+
+function paginateProducts(page) {
+  const productsPerPage = 10
+  const offset = productsPerPage * (page - 1)
+  knexInstance
+    .select('product_id', 'name', 'price', 'category')
+    .from('amazong_products')
+    .limit(productsPerPage)
+    .offset(offset)
+    .then(result => {
+      console.log(result)
+    })
+}
+
+paginateProducts(2)
+getProductsWithImages()
